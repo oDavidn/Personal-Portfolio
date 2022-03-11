f666446738691197b0f840e59a2d46d8348be102
davi nunes <davi@jomashop.io>
2022-03-11 17:21:24 -0300

    starter commit

---
+++ src/components/header/header.styled.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderStyled = styled.div`
+  background: gray;
+`
+export default HeaderStyled;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import HeaderStyled from "./header.styled";
+
+const Header = () =>
+{
+    return <HeaderStyled><div>THIS IS THE HEADER</div></HeaderStyled>
+}
+
+export default Header
